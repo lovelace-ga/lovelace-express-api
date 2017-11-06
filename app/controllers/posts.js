67e1729863c9f5a822d5756d39ee4fdac5b9d73c
dianamcnulty <dianamcnulty@gmail.com>
2017-11-06 14:15:41 -0500

    Add delete action
    -update still not working, 2 versions are commented out.

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -59,45 +59,71 @@ const create = (req, res, next) => {
 
 // const update = (req, res, next) => {
 //   const updatedPost = req.body.post
+//   console.log('req.post is', req.post)
 //   const findSite = function () {
-//     return Site.findOne({ _owner: req.user.id })
+//     return Site.find({ _owner: req.user.id })
 //   }
-//   const changePost = function () {
-//     return Post.update(updatedPost)
-//   }
-//   Promise.all([findSite(), changePost()])
-//     .then((values) => {
-//       values[0].blog.push(values[1].toJSON())
-//       console.log('values is', values)
-//       console.log('post is', values[1].toJSON())
-//       values[0].save()
-//       console.log('values[0]is ', values[0])
-//       return values[1]
+//   findSite()
+//     .then((site) => {
+//       console.log('site is', site)
+//       site.blog.update({$elemMatch: {id: req.post.id}}, {$set: {'$.title': req.body.post.title}})
+//       // site.blog._id(req.post.id)
+//
+//       site.save()
+//       console.log('site after saving is', site)
+//       return site
 //     })
+//
 //     .then(() => res.sendStatus(204))
 //     .catch(next)
+//   // const changePost = function () {
+//   //   return Post.update(updatedPost)
+//   // }
+//   // Promise.all([findSite(), changePost()])
+//   //   .then((values) => {
+//   //     const currentPost = values[0].blog.id(req.post.id)
+//   //     // values[0].blog.push(values[1].toJSON())
+//   //     console.log('values[0]is ', values[0])
+//   //     console.log('values[1]is', values[1])
+//   //     values[0].save()
+//   //     return values[1]
+//   //   })
 // }
+
 // const update = (req, res, next) => {
 //   delete req.body.post._owner  // disallow owner reassignment.
 //   const findSite = function () {
 //     console.log('findSite returns:', Site.findOne({ _owner: req.user.id }))
 //     return Site.findOne({ _owner: req.user.id })
 //   }
-//   findSite()
-  //
-  // req.post.update(req.body.post)
-  //   .then((response) => {
-  //     // site.save()
-  //     return response
-  //   })
-  //   .then(() => res.sendStatus(204))
-  //   .catch(next)
+//   req.post.update(req.body.post)
+//     .then((response) => {
+//       const site = findSite()
+//       site.save()
+//       return response
+//     })
+//     .then(() => res.sendStatus(204))
+//     .catch(next)
 // }
 
 const destroy = (req, res, next) => {
-  req.post.remove()
+  const findSite = function () {
+    // console.log('site is', Site.findOne({ _owner: req.user.id }))
+    return Site.findOne({ _owner: req.user.id })
+  }
+  findSite()
+    .then((site) => {
+      console.log('site is', site)
+      site.blog.id(req.post.id).remove()
+      site.save()
+      console.log('site after saving is', site)
+      return site
+    })
     .then(() => res.sendStatus(204))
     .catch(next)
+  // req.post.remove()
+  //   .then(() => res.sendStatus(204))
+  //   .catch(next)
 }
 
 module.exports = controller({
